9bd813bad0cf76308b31988b4588c987f8281ee5
Sridharan Dharmarajan <145624846+dharmasridhar@users.noreply.github.com>
2023-09-20 18:11:15 -0700

    Update streamlit_app.py
    Added more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast menu")
 streamlit.text( "Omega 3 & Blueberry oatmeal")
-
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('All Veggie pickles and multigrain sandwitch')

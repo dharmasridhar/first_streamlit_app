0c4e713e9ab3408c57313ae509fac3b72433ca15
Sridharan Dharmarajan <145624846+dharmasridhar@users.noreply.github.com>
2023-09-20 18:13:35 -0700

    Update streamlit_app.py
    Added emoticons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast menu")
-streamlit.text( "Omega 3 & Blueberry oatmeal")
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('All Veggie pickles and multigrain sandwitch')
+streamlit.text("🥣 🥗  🥑🍞")
+streamlit.text( "🥣Omega 3 & Blueberry oatmeal")
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 All Veggie pickles and multigrain sandwitch')
